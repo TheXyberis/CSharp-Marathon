be6b8d455e842484f4a3a6ebf63120191370d2bd
TheXyberis <xyberis.work@gmail.com>
2026-02-07 00:02:14 +0100

    feat: initial Hangman implementation with life system and letter tracking

---
+++ 06-HangmanGame/06-HangmanGame/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_HangmanGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "hello";
+
+            int maxLives = 7;
+            int currentLives = maxLives;
+
+            bool win = false;
+
+            List<char> guessedLetters = new List<char>();
+
+            while (currentLives > 0 && !win)
+            {
+                foreach (char c in word)
+                {
+                    if (guessedLetters.Contains(c))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(c);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write("_");
+                    }
+                }
+                Console.WriteLine("\nPlease guess a letter!");
+                Console.WriteLine($"{currentLives}/{maxLives} lives remaining");
+
+                char guess = Convert.ToChar(Console.ReadLine());
+
+                if (word.Contains(guess) && !guessedLetters.Contains(guess))
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Correct");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Incorrect");
+                    Console.ResetColor();
+                    currentLives--;
+                }
+                guessedLetters.Add(guess);
+
+                bool wordComplete = true;
+                foreach (char c in word)
+                {
+                    if (!guessedLetters.Contains(c))
+                    {
+                        wordComplete = false;
+                    }
+                }
+                win = wordComplete;
+            }
+            if (win)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Congratulations, you win!");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You lose...");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ 06-HangmanGame/06-HangmanGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("06-HangmanGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("06-HangmanGame")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4187c641-7562-4b2c-aa7f-9bb9f708d7bb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
